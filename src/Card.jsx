60982d664f1d84387fb9f501b7275d0ebf372cb4
Divyam <divyamsharma822@gmail.com>
2022-07-01 17:55:40 +0530

    updated

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Card.scss';
+
+const Card = ({ picurl, title }) => {
+    return (
+        <div className='card'>
+            <img className='pic' src={picurl} alt='' />
+            <div className='title'>{title}</div>
+        </div>
+    );
+};
+
+export default Card;
